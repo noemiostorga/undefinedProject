bc531f66e6325c3bc61dd4126afc9d80828357c4
noemiostorga <noemiostorga@gmail.com>
2015-11-19 16:10:37 -0800

    funderDetail ajax calls

--- js/components/funderDetail.jsx
+++ js/components/funderDetail.jsx
@@ -1,5 +1,6 @@
 var React = require('react'); 
 var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
 
 var FunderDetail = React.createClass({
 	render: function () {
@@ -7,9 +8,9 @@ var FunderDetail = React.createClass({
 		return(	
 			<div>
 				<ul>
-					<li><img /> picture</li>
+					<li><img id="pics"/> picture</li>
 					<li>title</li>
-					<li>amount</li>
+					<li id="price">amount</li>
 					<li>url</li>
 					<label><input /></label>
 					<li><button>submit</button></li>
@@ -25,3 +26,38 @@ var FunderDetail = React.createClass({
 
 
 module.exports = FunderDetail; 
+
+var DetFundDet = Backbone.Model.extend ({
+	
+	initialize: function () {
+		console.log("A new DetFundDet model  was made");
+	}
+
+});
+
+var FundDet = Backbone.Collection.extend({
+	url: "https://afternoon-scrubland-9189.herokuapp.com/api/lists/"
+});
+
+var fundDet = new FundDet();
+fundDet.fetch({
+	success: function(resp){
+		console.log(resp.toJSON);
+	},
+	error: function(error){
+		console.log(err);
+	}
+});
+
+var testDet = new FundDet();
+testDet.set({
+	'image' : 'image',
+	'name' : 'name'
+
+ 
+});
+
+
+
+
+
--- js/components/listDetail.jsx
+++ js/components/listDetail.jsx
@@ -39,14 +39,10 @@ var DetFundView = Backbone.Model.extend({
 		}
 		
 });
-
 var FundView = Backbone.Collection.extend({
-
 	url: "https://afternoon-scrubland-9189.herokuapp.com/api/lists/"
 });
-
 var fundView = new FundView();
-
 fundView.fetch({
 	success: function(resp) {
 		console.log(resp.toJSON());
@@ -55,16 +51,12 @@ fundView.fetch({
 		console.log(err);
 	}
 });
-
 var test = new FundView();
-
 test.set({
 	'title': 'test',
 	'user': 2,
 	
 });
-
-
 
 
 
